cce40a5b3aedd2176599ffb436ba68131ab9db3d
nmdoa <nmdoa@protonmail.ch>
2021-01-28 20:10:09 +0200

    add jsx files to prettier

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Pagination from '../components/Pagination/Pagination'
 
@@ -6,25 +6,29 @@ import { ADD_USER, LIST_USERS, DELETE_USER, UPDATE_USER } from '../graphql/user'
 import Modal from '../components/Modal'
 import UserAddForm from '../components/UserAddForm'
 import List from '../components/List'
-import {useCrud} from "../hooks/CRUD";
-import {useList} from "../hooks/list";
-import {useUserForm} from "../hooks/userForm";
+import { useCrud } from '../hooks/CRUD'
+import { useList } from '../hooks/list'
+import { useUserForm } from '../hooks/userForm'
 
 const listSchema = [0, 5, 5, 2]
 
 const Users = () => {
     const [isModalShown, setIsModalShown] = useState(false)
-    const [addUser, updateUser, deleteUser, {loading: loadingCrud}] = useCrud(ADD_USER, UPDATE_USER, DELETE_USER)
+    const [addUser, updateUser, deleteUser, { loading: loadingCrud }] = useCrud(
+        ADD_USER,
+        UPDATE_USER,
+        DELETE_USER
+    )
     const [
         loading,
         error,
         data,
         refetch,
-        {skipCount, setSkipCount},
-        {paginationLimit}
+        { skipCount, setSkipCount },
+        { paginationLimit },
     ] = useList(LIST_USERS)
 
-    const {email, password, handleSubmit, handleUpdate} = useUserForm(
+    const { email, password, handleSubmit, handleUpdate } = useUserForm(
         async (email, password) => {
             const { data, error } = await addUser({
                 variables: { email, password },
@@ -37,7 +41,8 @@ const Users = () => {
                 refetch()
                 return true
             }
-        }, async (email, password, id) => {
+        },
+        async (email, password, id) => {
             const { data, error } = await updateUser({
                 variables: {
                     id,
@@ -54,7 +59,8 @@ const Users = () => {
                 refetch()
                 return true
             }
-        })
+        }
+    )
 
     const handleOnDelete = async (id) => {
         if (window.confirm('Are you sure you want to Delete selected user?')) {
@@ -73,7 +79,10 @@ const Users = () => {
             <List
                 model={users}
                 onDelete={handleOnDelete}
-                onUpdate={(data) => {handleUpdate(data); setIsModalShown(true) }}
+                onUpdate={(data) => {
+                    handleUpdate(data)
+                    setIsModalShown(true)
+                }}
                 schema={listSchema}
             />
             <div className="row">
@@ -107,7 +116,11 @@ const Users = () => {
                 onCancel={() => setIsModalShown(false)}
                 loading={loadingCrud}
             >
-                <UserAddForm email={email} password={password} handleSubmit={handleSubmit} />
+                <UserAddForm
+                    email={email}
+                    password={password}
+                    handleSubmit={handleSubmit}
+                />
             </Modal>
         </React.Fragment>
     )
